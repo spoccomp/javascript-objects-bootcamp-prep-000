9a09e2ad029b6515696edfbf85e2c7d08115d68a
Richard Trapanese <richardtrapanese@gmail.com>
2018-03-14 04:50:16 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 function removeFromPlaylist(playlist, artistName){
   //removes `artistName` from `playlist`
-  delete playlist.artistName; // true
+  delete playlist[artistName]; // true
   return playlist;
 }
