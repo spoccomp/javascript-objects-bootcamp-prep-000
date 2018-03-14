f760468c9e981f3200098e1db98068076269fa74
Richard Trapanese <richardtrapanese@gmail.com>
2018-03-14 04:14:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = new Object ({Eagles: 'Alright'});
+var playlist = new Object ({Eagles: 'Alright', Doors: 'Brown Eye Girl'});
 function updatePlaylist(playlist, artistName, songTitle){
   //adds the `artistName: songTitle` key-value pair to `playlist`
   playlist[artistName] = songTitle;
